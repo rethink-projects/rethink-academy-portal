42c8ba6f7e6217389842c931a716ee3d67fa67d6
FelipeReggiane <felipe.reggiane@rethink.dev>
2022-07-12 13:45:26 -0300

    textarea felipe-reggiane

--- src/screens/playground/PlaygroundScreen.tsx
+++ src/screens/playground/PlaygroundScreen.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "../../components/Button/Button";
+import Textarea from "../../components/Textarea/Textarea";
 import styles from "./Playground.module.css";
 
 function PlaygroundScreen() {
@@ -12,6 +13,14 @@ function PlaygroundScreen() {
       <div className={styles.sub_content}>
         <span>Componente Button</span>
         <Button />
+      </div>
+      <div className={styles.sub_content}>
+        <span>Componente Textarea</span>
+        <div className={styles.sub_content_textarea}>
+          <Textarea placeholder="Place Holder" label="Large" caption="Element caption" type="large" />
+          <Textarea placeholder="Place Holder" label="Default" caption="Element caption" disabled />
+          <Textarea placeholder="Place Holder" label="Small" caption="Element caption" type="small" />
+        </div>
       </div>
     </div>
   );
