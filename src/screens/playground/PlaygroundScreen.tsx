8594e3156c94df8c52e8e39d165381bb2d41bfd0
Filipe Prado <filipe.prado@rethink.dev>
2022-07-12 17:27:19 -0300

    corrigindo detalhes no layout do playground e componentes

--- src/screens/playground/PlaygroundScreen.tsx
+++ src/screens/playground/PlaygroundScreen.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Button from "../../components/Button/Button";
+import SocialButton from "../../components/SocialButton/SocialButton";
+import Spinner from "../../components/Spinner/Spinner";
 import styles from "./Playground.module.css";
 
 function PlaygroundScreen() {
@@ -7,11 +8,15 @@ function PlaygroundScreen() {
     <div className={styles.playground_container}>
       <div className={styles.main_content}>
         <h2>Playground de Componentes</h2>
-        <p>insira abaixo o seu componente:</p>
+        <p>Insira abaixo o seu componente:</p>
       </div>
       <div className={styles.sub_content}>
-        <span>Componente Button</span>
-        <Button />
+        <SocialButton type='secundary' onClick={() => console.log("clicou")} />
+        <SocialButton onClick={() => console.log("clicou")} />
+      </div>
+      <div className={styles.sub_content}>
+        <Spinner size='big' type='light' isLoading={true} />
+        <Spinner size='small' type='light' isLoading={true} />
       </div>
     </div>
   );
