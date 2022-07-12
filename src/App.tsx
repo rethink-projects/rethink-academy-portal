c3624dc0b51d2192f868f8242f2c343ddbebc8b8
anaclara-rethink <102759771+anaclara-rethink@users.noreply.github.com>
2022-07-12 14:28:17 -0300

    Voltando com o App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SocialButton from "./components/SocialButton/SocialButton";
 import HomeScreen from "./screens/home/HomeScreen";
 import PlaygroundScreen from "./screens/playground/PlaygroundScreen";
 
 function App() {
   return (
-    <div>
-      <h1>Social Button</h1>
-      <SocialButton type="primary" onClick={() => {}} />
-      <SocialButton type="secundary" onClick={() => {}} />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/playground" element={<PlaygroundScreen />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/IconButton/IconButton.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./IconButton.module.css";
+
+export const IconButton = () => {
+  return <div>IconButton</div>;
+};
