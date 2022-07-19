e0f7bebbae00fceb4d85bb1e18decccb44de3ed3
FelipeReggiane <felipe.reggiane@rethink.dev>
2022-07-19 14:23:50 -0300

    Feature Modal

--- src/screens/playground/PlaygroundScreen.tsx
+++ src/screens/playground/PlaygroundScreen.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../../components/Button/Button";
 import Checkbox from "../../components/Checkbox/Checkbox";
 import styles from "./Playground.module.css";
 import Radio from "../../components/Radio/Radio";
+import Modal from "../../components/Modal/Modal";
 
 function PlaygroundScreen() {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+
   return (
     <div className={styles.playground_container}>
       <div className={styles.main_content}>
@@ -16,6 +20,14 @@ function PlaygroundScreen() {
         <Checkbox name="Teste" />
         <Checkbox name="Não se atreva a me marcar" disabled />
         <Checkbox name="Marque para ser feliz" disabled checked />
+      </div>
+      <div className={styles.sub_content}>
+        <span>Modal</span>
+        <button onClick={() => setModalOpen(true) } >Open Modal </button>
+        {isModalOpen && 
+          <Modal isModalOpen={isModalOpen} title="Title" description="If you think it's a modal dialog, please hit the confirm button to let us know!" onClose={() => setModalOpen(false) } nameCheckbox="Don't show again"  >
+            slot
+          </Modal>}
       </div>
     </div>
   );
