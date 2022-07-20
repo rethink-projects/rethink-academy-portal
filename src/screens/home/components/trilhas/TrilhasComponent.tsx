04f77c8bcedeb4d8b1ccb448af776876e91d0ced
FernandoHenrique2001 <fernandohenrique.gp@gmail.com>
2022-07-20 18:03:57 -0300

    Implementando o layout

--- src/screens/home/components/trilhas/TrilhasComponent.tsx
+++ src/screens/home/components/trilhas/TrilhasComponent.tsx
@@ -1,14 +1,21 @@
 import styles from "./TrilhasComponent.module.css"
 import IconMap from '@mui/icons-material/MapOutlined';
+import CardTrilhasHome from "./trilhasSubComponents/CardTrilhasHome";
 
+type TrilhasComponentType = {
+    name: string;
+    dataInput: Object;
+}
 const TrilhasComponent = () => {
     return (
         <div className={styles.trilhas_container}>
             <div className={styles.trilhas_title_container}>
-                <div className={styles.icon_container}><IconMap /></div>
+                <div className={styles.icon_container}><IconMap
+                // style={{ width: "16.25px", height: "16.25px" }} 
+                /></div>
                 <span >Trilhas</span>
             </div>
-            <div>CARDS</div>
+            <CardTrilhasHome />
         </div>
     )
 }
--- src/screens/home/components/trilhas/trilhasSubComponents/CardTrilhasHome.tsx
+++ src/screens/home/components/trilhas/trilhasSubComponents/CardTrilhasHome.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import styles from "./CardTrilhasHome.module.css";
+import IconPadlock from '@mui/icons-material/LockOutlined';
 
+type CardTrilhasHome = {
+    name: string;
+    dataInput: Object
+}
 const CardTrilhasHome = () => {
     return (
         <div className={styles.container}>
-
+            <span>Academy</span>
+            <div className={styles.divisoria}></div>
+            <div className={styles.state}>
+                {/* progress bar */}
+                <div className={styles.blocked}>
+                    <div className={styles.padlock_border}>
+                        <IconPadlock />
+                    </div>
+                    <span>Bloqueada!</span>
+                </div>
+            </div>
         </div>
     )
 }
