b1469328bda5ab9ed40afce887f9e5a5e1bd2da2
FernandoHenrique2001 <fernandohenrique.gp@gmail.com>
2022-07-20 09:25:50 -0300

    ajustando css

--- src/components/CardTrilhas/CardTrilhas.tsx
+++ src/components/CardTrilhas/CardTrilhas.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from "./CardTrilhas.module.css";
+import image from "../../assets/academyCardTrilhas.png";
 
 type TypeCardTrilhas = {
     title: string;
@@ -9,13 +10,17 @@ type TypeCardTrilhas = {
 const CardTrilhas = ({ title, description }: TypeCardTrilhas) => {
     return (
         <div className={styles.container}>
-            <div className={styles.card_image}>
-                <img src="src\assets\academyCardTrilhas.png" />
-            </div>
-            <div className={styles.card_content}>
-                <h1 className={styles.card_content_title}>{title}</h1>
-                <p className={styles.card_content_description}>{description}</p>
-                <p>Substitua essa tag pela barra de progresso</p>
+            <div className={styles.container_inner}>
+                <div className={styles.card_image}>
+                    <img src={image} />
+                </div>
+                <div className={styles.card_content}>
+                    <div className={styles.card_content_inner}>
+                        <h1 className={styles.card_content_title}>{title}</h1>
+                        <p className={styles.card_content_description}>{description}</p>
+                        <p>Substitua essa tag pela barra de progresso</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
