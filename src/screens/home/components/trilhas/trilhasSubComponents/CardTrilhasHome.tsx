d259f2c2bc161d5dcee04d76cfad3df49cec7fff
Lucaspaula6 <lucas.paula@rethink.dev>
2022-07-22 14:52:23 -0300

    Comit antes de merge

--- src/screens/home/components/trilhas/trilhasSubComponents/CardTrilhasHome.tsx
+++ src/screens/home/components/trilhas/trilhasSubComponents/CardTrilhasHome.tsx
@@ -1,27 +1,29 @@
-import React from 'react'
+import React from "react";
 import styles from "./CardTrilhasHome.module.css";
-import IconPadlock from '@mui/icons-material/LockOutlined';
+import IconPadlock from "@mui/icons-material/LockOutlined";
+import ProgressBar from "../../../../../components/ProgressBar/ProgressBar";
 
 type CardTrilhasHome = {
-    name: string;
-    dataInput: Object
-}
+  name: string;
+  dataInput: Object;
+};
 const CardTrilhasHome = () => {
-    return (
-        <div className={styles.container}>
-            <span>Academy</span>
-            <div className={styles.divisoria}></div>
-            <div className={styles.state}>
-                {/* progress bar */}
-                <div className={styles.blocked}>
+  return (
+    <div className={styles.container}>
+      <span>Academy</span>
+      <div className={styles.divisoria}></div>
+      <div className={styles.state}>
+        <ProgressBar />
+
+        {/* <div className={styles.blocked}>
                     <div className={styles.padlock_border}>
                         <IconPadlock />
                     </div>
                     <span>Bloqueada!</span>
-                </div>
-            </div>
-        </div>
-    )
-}
+                </div> */}
+      </div>
+    </div>
+  );
+};
 
-export default CardTrilhasHome
+export default CardTrilhasHome;
--- src/screens/playground/PlaygroundScreen.tsx
+++ src/screens/playground/PlaygroundScreen.tsx
@@ -12,13 +12,11 @@ function PlaygroundScreen() {
   return (
     <div className={styles.playground_container}>
       <div className={styles.main_content}>
+        <h1>Manter essa tela Limpa, após criar o componente</h1>
         <h2>Playground de Componentes</h2>
         <p>Insira abaixo o seu componente:</p>
-        <div className={styles.sub_content}>
+        <div className={styles.playground_container_inner}>
           <TrilhasComponent />
-          <div className={styles.playground_container_inner}>
-            <h1>Manter essa tela Limpa, após criar o componente</h1>
-          </div>
         </div>
       </div>
     </div>
