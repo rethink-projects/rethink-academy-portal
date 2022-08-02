d8d4e1aac963889e8cd5d9dbee1adf5ee7640ff8
Gabriel Gomes <gabriel.gomes@rethink.dev>
2022-08-02 12:50:28 -0300

    chore

--- src/screens/playground/PlaygroundScreen.tsx
+++ src/screens/playground/PlaygroundScreen.tsx
@@ -5,7 +5,6 @@ function PlaygroundScreen() {
     <div className={styles.playground_container}>
       <div className={styles.playground_container_inner}>
         <h1>Manter essa tela Limpa, após criar o componente</h1>
-        
       </div>
     </div>
   );
