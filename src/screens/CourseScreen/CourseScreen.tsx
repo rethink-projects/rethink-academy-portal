01adea8968dbe307fb9020f5a1fc35e201f97744
FernandoHenrique2001 <fernandohenrique.gp@gmail.com>
2022-08-03 17:17:25 -0300

    Implementando Card de informações

--- src/screens/CourseScreen/CourseScreen.tsx
+++ src/screens/CourseScreen/CourseScreen.tsx
@@ -2,6 +2,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import Breadcrumb from "../../components/Breadcrumb/Breadcrumb";
 import styles from "./CourseScreen.module.css";
 import Acordeon from "../course/components/acordeon/Acordeon";
+import CardInfoCurso from "../course/components/card/CardInfoCurso";
 
 const CourseScreen = () => {
     const location = useLocation();
@@ -67,7 +68,7 @@ const CourseScreen = () => {
         const linkHome = { title: "Home", link: "/" };
         const linkTrilhas = { title: "Trilhas", link: "/trilhas" };
         const linkCourses = { title: "Cursos", link: path[0] };
-        const linkCourse = { title: "Curso", link: url };
+        const linkCourse = { title: "Curso 1", link: url };
         return [linkHome, linkTrilhas, linkCourses, linkCourse];
     }
 
@@ -76,7 +77,7 @@ const CourseScreen = () => {
             <div className={styles.inner_container}>
                 <div className={styles.content_course}>
                     <Breadcrumb breadcrumbItems={getBreadcrumbs()} />
-                    <h1 className={styles.title}>title</h1>
+                    <h1 className={styles.title}>UX Design</h1>
                     <h2 className={styles.about}>Sobre o Curso:</h2>
                     <p className={styles.description}>
                         Aprenda a executar pesquisas de UX Design, fazer testes de usabilidade e elaborar análises, além de utilizar frameworks e métodos para a criação de designs de qualidade e que ofereçam uma boa experiência ao usuário. Ao concluir as aulas, você estará pronto para definir processos e construir frameworks baseados em estudos sobre as necessidades dos usuários, seus objetivos, habilidades e limitações, para alcançar os objetivos de negócios.
@@ -88,7 +89,9 @@ const CourseScreen = () => {
                     </div>
                 </div>
                 <div className={styles.practical_information}>
-
+                    <div className={styles.card_info}>
+                        <CardInfoCurso />
+                    </div>
                 </div>
             </div>
         </div>
