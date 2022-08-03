e6d81cc17cd09a82c2a0d9eb62a172edc3f61a6b
anaclara-rethink <102759771+anaclara-rethink@users.noreply.github.com>
2022-08-03 11:13:17 -0300

    resolvendo conflitos

--- src/screens/playground/PlaygroundScreen.tsx
+++ src/screens/playground/PlaygroundScreen.tsx
@@ -1,7 +1,3 @@
-import { useAuth } from "../../context/AuthContext";
-import CardInfoCurso from "../course/components/card/CardInfoCurso";
-import CardCurso from "../course/components/card/CardInfoCurso";
-import Acordeon from "../course/components/acordeon/Acordeon";
 import styles from "./Playground.module.css";
 
 function PlaygroundScreen() {
@@ -9,7 +5,6 @@ function PlaygroundScreen() {
     <div className={styles.playground_container}>
       <div className={styles.playground_container_inner}>
         <h1>Manter essa tela Limpa, após criar o componente</h1>
-        <CardInfoCurso />
       </div>
     </div>
   );
