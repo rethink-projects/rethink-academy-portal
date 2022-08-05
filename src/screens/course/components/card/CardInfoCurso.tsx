b7337c59884b606153af20c83c442f8153d81968
anaclara-rethink <102759771+anaclara-rethink@users.noreply.github.com>
2022-08-05 18:03:02 -0300

    Card concluído

--- src/screens/course/components/card/CardInfoCurso.tsx
+++ src/screens/course/components/card/CardInfoCurso.tsx
@@ -4,39 +4,83 @@ import Avatar from "../../../../components/Avatar/Avatar";
 import Badge from "../../../../components/Badge/Badge";
 import styles from "./CardInfoCurso.module.css";
 
-const CardInfoCurso = () => {
+type CardInfoProps = {
+  nivel: "Iniciante" | "Intermediário" | "Avançado";
+  // na primeira posição do vetor voce envia o numero de modulos, na segunda posição voce envia o numero de aulas
+  module_class: { module: number; class: number };
+  author: string;
+  authorDescription: string;
+  learn: string[];
+  skills: string[];
+};
+
+const CardInfoCurso = ({
+  nivel,
+  module_class,
+  author,
+  authorDescription,
+  learn,
+  skills,
+}: CardInfoProps) => {
   return (
     <div className={styles.card_container}>
       <h1 className={styles.title}>Informações Práticas</h1>
       <div className={styles.informacoes_praticas}>
         <div className={styles.info_nivel}>
           <Badge
-            icon={<img src={Images.icons.Batery} />}
+            icon={
+              nivel === "Iniciante" ? (
+                <img src={Images.icons.weakBatery} />
+              ) : nivel === "Intermediário" ? (
+                <img src={Images.icons.averageBatery} />
+              ) : (
+                <img src={Images.icons.fullBatery} />
+              )
+            }
             color="secondary"
             size="large"
           />
-          <h1 className={styles.title}>Nível iniciante</h1>
+          <h1 className={styles.title}>Nível {nivel}</h1>
         </div>
         <div className={styles.info_nivel}>
           <Badge icon={<AccessTime />} size="large" />
           <div className={styles.info_description}>
-            <h1 className={styles.title}>Carga horária: 120h</h1>
-            <p>8 Módulos e 35 Aulas</p>
+            <h1 className={`${styles.title} ${styles.title_space}`}>
+              Carga horária: 120h
+            </h1>
+            <p>
+              {module_class.module} Módulos e {module_class.class} Aulas
+            </p>
           </div>
         </div>
         <div className={styles.info_nivel}>
-          <Avatar type="image" children={<AccessTime />} size="large" />
-          {/* <img src={user.avatarUrl} alt="avatar do user" /> */}
+          <Avatar
+            type="image"
+            children={
+              <img src="https://st.depositphotos.com/1010338/2099/i/600/depositphotos_20999947-stock-photo-tropical-island-with-palms.jpg" />
+            }
+            size="large"
+          />
           <div className={styles.info_description}>
-            <h1 className={styles.title}>Marcela Monteiro</h1>
-            <p>Diretora de arte e designer gráfica</p>
+            <h1 className={`${styles.title} ${styles.title_space}`}>
+              {author}
+            </h1>
+            <p>{authorDescription}</p>
           </div>
         </div>
       </div>
-      <h1 className={styles.title}>O que você vai aprender neste curso:</h1>
+      <h1 className={`${styles.title} ${styles.title_space}`}>
+        O que você vai aprender neste curso:
+      </h1>
       <div className={styles.topicos}>
         <ul className={styles.list}>
-          <li>
+          {!!learn.length &&
+            learn.map((title) => (
+              <li>
+                <p>{title}</p>
+              </li>
+            ))}
+          {/* <li>
             <p>Metodologia Double Diamond</p>
           </li>
           <li>
@@ -47,13 +91,21 @@ const CardInfoCurso = () => {
           </li>
           <li>
             <p>Documentação de processos</p>
-          </li>
+          </li> */}
         </ul>
       </div>
-      <h1 className={styles.title}>Habilidades que você terá:</h1>
+      <h1 className={`${styles.title} ${styles.title_space}`}>
+        Habilidades que você terá:
+      </h1>
       <div>
         <ul className={styles.list}>
-          <li>
+          {!!skills.length &&
+            skills.map((title) => (
+              <li>
+                <p>{title}</p>
+              </li>
+            ))}
+          {/* <li>
             <p>Produzir e aplicar um Teste de Usabilidade</p>
           </li>
           <li>
@@ -64,7 +116,7 @@ const CardInfoCurso = () => {
           </li>
           <li>
             <p>Preparar um Hand-off</p>
-          </li>
+          </li> */}
         </ul>
       </div>
     </div>
--- src/screens/playground/PlaygroundScreen.tsx
+++ src/screens/playground/PlaygroundScreen.tsx
@@ -6,7 +6,16 @@ function PlaygroundScreen() {
     <div className={styles.playground_container}>
       <div className={styles.playground_container_inner}>
         <h1>Manter essa tela Limpa, após criar o componente</h1>
-        <CardInfoCurso />
+        <CardInfoCurso
+          nivel="Avançado"
+          module_class={{ module: 2, class: 4 }}
+          author="Marcela Monteiro"
+          authorDescription="Diretora de arte e designer gráfica"
+          learn={["blabl", "biruleivbe", "asdas", "gabrieu"]}
+          skills={["blabl", "biruleivbe", "asdas", "gabrieu"]}
+          // autor="ANa"
+          // description="Diretora dos tops"
+        />
       </div>
     </div>
   );
