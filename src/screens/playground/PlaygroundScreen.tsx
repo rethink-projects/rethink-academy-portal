65bb2b56d92235e6aa90bb42431efcba01cecfd0
Lucaspaula6 <lucas.paula@rethink.dev>
2022-08-09 10:06:22 -0300

    Update Playground.tsx

--- src/screens/playground/PlaygroundScreen.tsx
+++ src/screens/playground/PlaygroundScreen.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import Dropdown from "../../components/Dropdown/Dropdown";
 import styles from "./Playground.module.css";
 
 function PlaygroundScreen() {
@@ -10,36 +9,6 @@ function PlaygroundScreen() {
     <div className={styles.playground_container}>
       <div className={styles.main_content}>
         <h1>Manter essa tela Limpa, após criar o componente</h1>
-        <Dropdown
-          id={"ss"}
-          options={options}
-          setValue={setValue}
-          size={"micro"}
-        />
-        <Dropdown
-          id={"s"}
-          options={options}
-          setValue={setValue}
-          size={"small"}
-        />
-        <Dropdown
-          id={"sss"}
-          options={options}
-          setValue={setValue}
-        />
-        <Dropdown
-          id={"ssss"}
-          options={options}
-          setValue={setValue}
-          size={"large"}
-        />
-        <Dropdown
-          id={"ssss"}
-          options={options}
-          setValue={setValue}
-          size={"large"}
-          disabled
-        />
       </div>
     </div>
   );
