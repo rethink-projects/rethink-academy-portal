a4b6441d141233b3efefc74da09df46aa6b7df14
amandadclsRethink <amanda.duarte@rethink.dev>
2022-08-11 06:26:38 -0300

    Card Metas completo

--- src/components/CardMetas/CardMetas.tsx
+++ src/components/CardMetas/CardMetas.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 
 import styles from "./CardMetas.module.css";
 
@@ -9,24 +9,113 @@ import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRound
 import ProgressBar from "../ProgressBar/ProgressBar";
 
 type cardProps = {
-  goalsData: Array<goalsData>;
+  id: number;
   month: string;
+  isActive: boolean;
+  goalsData: goalsData[];
 };
 
 type goalsData = {
+  id: number;
   title: string;
-  month: string;
   isDone: boolean;
 };
 
-const CardMetas = ({ goalsData, month }: cardProps) => {
-  const monthsGoals = goalsData.filter(
-    (goal) => goal.month.toLowerCase() === month.toLowerCase()
-  );
-  const goalsQuantity = monthsGoals.length;
+const goalsDataSim = [
+  {
+    id: 1,
+    month: "Janeiro",
+    isActive: true,
+    goalsData: [
+      { title: "meta 1", isDone: true, id: 1 },
+      { title: "meta 2", isDone: false, id: 2 },
+      { title: "meta 3", isDone: true, id: 3 },
+      { title: "meta 4", isDone: false, id: 4 },
+      { title: "meta 5", isDone: true, id: 5 },
+      { title: "meta 6", isDone: false, id: 6 },
+      { title: "meta 7", isDone: false, id: 7 },
+      { title: "meta 8", isDone: true, id: 8 },
+      { title: "meta 9", isDone: false, id: 9 },
+      { title: "meta 10", isDone: false, id: 10 },
+      { title: "meta 11", isDone: true, id: 11 },
+      { title: "mes errado", isDone: true, id: 12 },
+      {
+        title: "mes certo, escrita diferente ",
+        isDone: false,
+        id: 13,
+      },
+      // {
+      //   title:
+      //     "Nome de meta muito grande pra ver como vai ficar a exibição wee =D ......... 1 2 3 4 5 6 7 8 9 10 %¨$&#$%¨esse trem n dá 3 linhas e deu ruim com 3, ver se piora com mais",
 
-  let completedGoals = monthsGoals.filter((goal) => goal.isDone === true);
-  let uncompletedGoals = monthsGoals.filter((goal) => goal.isDone === false);
+      //   isDone: false,
+      //   id: 14,
+      // },
+    ],
+  },
+  {
+    id: 2,
+    month: "Outubro",
+    isActive: false,
+    goalsData: [
+      { id: 1, title: "asdfg", isDone: true },
+      { id: 2, title: "qwerty", isDone: false },
+    ],
+  },
+];
+
+const CardMetas = () => {
+  const [goals, setGoals] = useState<cardProps[]>(goalsDataSim);
+
+  const handleClick = (props: any) => {
+    setGoals(() =>
+      goals.map((goal: any) => {
+        if (goal.isActive) {
+          return {
+            ...goal,
+            goalsData: goal.goalsData.map((goalsData: any) => {
+              if (goalsData.id === props.id) {
+                return {
+                  ...goalsData,
+                  isDone: props.props,
+                };
+              } else {
+                return {
+                  ...goalsData,
+                };
+              }
+            }),
+          };
+        } else {
+          return {
+            ...goal,
+          };
+        }
+      })
+    );
+  };
+
+  let activeGoals: cardProps = {
+    id: 0,
+    month: "",
+    isActive: true,
+    goalsData: [{ id: 0, title: "", isDone: false }],
+  };
+
+  goals.map((monthsGoals: any) => {
+    if (monthsGoals.isActive) {
+      activeGoals = monthsGoals;
+    }
+  });
+
+  const goalsQuantity = activeGoals.goalsData.length;
+
+  let completedGoals = activeGoals.goalsData.filter(
+    (goal) => goal.isDone === true
+  );
+  let uncompletedGoals = activeGoals.goalsData.filter(
+    (goal) => goal.isDone === false
+  );
 
   return (
     <div className={styles.CardUltimasMetas_info}>
@@ -34,7 +123,7 @@ const CardMetas = ({ goalsData, month }: cardProps) => {
         <img src={Images.MedalUltimasMetas} alt="medalha ultimas Goals" />
 
         <div className={styles.CardUltimasMetas_inner_info}>
-          <h1>Avaliação de {month}</h1>
+          <h1>Avaliação de {activeGoals.month}</h1>
           <p>
             {goalsQuantity < 10 ? `0${goalsQuantity}` : goalsQuantity} Metas
           </p>
@@ -47,36 +136,44 @@ const CardMetas = ({ goalsData, month }: cardProps) => {
       </div>
 
       <div className={styles.Metas_Container}>
-        {uncompletedGoals.map((value, index) => {
+        {uncompletedGoals.map((value) => {
           return (
             <>
               <Checkbox
                 name={value.title}
                 size="small"
-                isChecked={false}
-                setIsChecked={() => {}}
+                isChecked={value.isDone}
+                setIsChecked={(props) =>
+                  handleClick({
+                    id: value.id,
+                    props,
+                  })
+                }
+                key={value.id}
               />
             </>
           );
         })}
-        {completedGoals.map((value, index) => {
+        {completedGoals.map((value) => {
           return (
             <>
-              <>
-                <Checkbox
-                  name={value.title}
-                  size="small"
-                  isChecked={true}
-                  setIsChecked={() => {}}
-                />
-              </>
+              <Checkbox
+                name={value.title}
+                size="small"
+                isChecked={value.isDone}
+                setIsChecked={(props) =>
+                  handleClick({
+                    id: value.id,
+                    props,
+                  })
+                }
+                key={value.id}
+              />
             </>
           );
         })}
       </div>
-
       <div className={styles.divisingLine}></div>
-
       <div className={styles.CardUltimasMetas_progress}>
         <div className={styles.progress_Goals}>
           <p>{completedGoals.length} </p>
--- src/screens/playground/PlaygroundScreen.tsx
+++ src/screens/playground/PlaygroundScreen.tsx
@@ -10,28 +10,7 @@ function PlaygroundScreen() {
         <p>Insira abaixo o seu componente:</p>
 
         <div className={styles.playground_container_inner}>
-          <CardMetas
-            goalsData={[
-              { title: "meta 1", month: "Janeiro", isDone: true },
-              { title: "meta 2", month: "Janeiro", isDone: false },
-              { title: "meta 3", month: "Janeiro", isDone: true },
-              { title: "meta 4", month: "Janeiro", isDone: false },
-              { title: "meta 5", month: "Janeiro", isDone: true },
-              { title: "meta 6", month: "Janeiro", isDone: false },
-              { title: "meta 7", month: "Janeiro", isDone: false },
-              { title: "meta 8", month: "Janeiro", isDone: true },
-              { title: "meta 9", month: "Janeiro", isDone: false },
-              { title: "meta 10", month: "Janeiro", isDone: false },
-              { title: "meta 11", month: "Janeiro", isDone: true },
-              { title: "mes errado", month: "Dezembro", isDone: true },
-              {
-                title: "mes certo, escrita diferente ",
-                month: "janeiro",
-                isDone: false,
-              },
-            ]}
-            month="Janeiro"
-          />
+          <CardMetas />
         </div>
       </div>
     </div>
