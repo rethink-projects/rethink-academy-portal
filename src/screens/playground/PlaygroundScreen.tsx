2262778a3c8f09aa0b9da290010fe8a401d970e3
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-08-11 10:38:56 -0300

    removendo componente do playground

--- src/screens/playground/PlaygroundScreen.tsx
+++ src/screens/playground/PlaygroundScreen.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Note from "../../components/Note/Note";
 import styles from "./Playground.module.css";
 
 function PlaygroundScreen() {
@@ -7,7 +6,6 @@ function PlaygroundScreen() {
     <div className={styles.playground_container}>
       <div className={styles.playground_container_inner}>
         <h1>Manter essa tela Limpa, após criar o componente</h1>
-        <Note />
       </div>
     </div>
   );
