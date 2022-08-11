1becbf02feb3ae6e132f7e868de17a7b405c402e
Gabriel Gomes <gabriel.gomes@rethink.dev>
2022-08-11 12:22:32 -0300

    fix accordion

--- src/components/AccordionMM/AccordionMM.tsx
+++ src/components/AccordionMM/AccordionMM.tsx
@@ -7,27 +7,30 @@ import styles from "./AccordionMM.module.css";
 import Images from "../../assets";
 
 const AccordionMM = () => {
-  const [active, setActive] = useState(true);
+  const [active, setActive] = useState(false);
 
   return (
-    <div className={styles.accordion_container}>
+    <div className={styles.accordion}>
       <div className={styles.accordion_blackBar}>
         <img src={Images.icons.IconEdit} alt="A Edit Icon" />
         <img src={Images.icons.deleteIcon} alt="" />
       </div>
       <div className={styles.accordion_body}>
-        <div className={styles.accordion_label}>
-          <div className={styles.accordion_labelLeft}>
+        <div
+          className={styles.accordion_label}
+          onClick={() => setActive(!active)}
+        >
+          <div className={styles.accordion_label_left}>
             <img src={Images.icons.chatIcon} alt="Chat Icon" />
             <p>Atividades do Estagiário</p>
           </div>
-          <div className={styles.accordion_labelRight}>
+          <div className={styles.accordion_label_right}>
             <p>1h45</p>
             <img src={Images.icons.arrowDownIcon} alt="Chat Icon" />
           </div>
         </div>
+        {active && <div className={styles.accordion_active}>Descrição</div>}
       </div>
-      {active && <div className={styles.accordion_active}>Descrição</div>}
     </div>
   );
 };
