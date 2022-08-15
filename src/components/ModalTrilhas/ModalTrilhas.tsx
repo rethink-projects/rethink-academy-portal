2f2b2129be7891634535fffa813da68005f9e681
FernandoHenrique2001 <fernandohenrique.gp@gmail.com>
2022-08-15 11:40:59 -0300

    First commit

---
+++ src/components/ModalTrilhas/ModalTrilhas.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useRef, RefObject, useState } from 'react'
+import IconButton from '../IconButton/IconButton';
+import styles from "./ModalTrilhas.module.css"
+import Checkbox from '../Checkbox/Checkbox';
+import { style } from '@mui/system';
+
+type modalProps = {
+    title: string;
+    iconClose?: JSX.Element;
+    onClose: VoidFunction;
+    id?: string;
+    onClickConfirm?: VoidFunction;
+}
+
+const Modal = ({ iconClose, title, onClose = () => { }, id = "outside", onClickConfirm }: modalProps) => {
+
+    const [disabledConfirm, setDisabledConfirm] = useState(false);
+    const disabledConfirmClass = disabledConfirm ? styles.modal_actions_confirm_disabled : "";
+
+    const handleOutsideClick = (e: any) => {
+        if (e.target.id === id) {
+            onClose();
+        }
+    }
+
+    // parte para mostrar o onClick do confirm.
+    const handleClickConfirm = () => {
+        console.log(true);
+    }
+
+    if (!onClickConfirm) {
+        onClickConfirm = handleClickConfirm;
+    }
+
+    return (
+        <div id={id} className={styles.modal_outside} onClick={handleOutsideClick}>
+            <div className={styles.modal_container}>
+                <div className={styles.modal_header} >
+                    <h1 className={styles.modal_title} >{title}</h1>
+                    <div onClick={onClose} className={styles.modal_header_icon}>
+                        {iconClose}
+                    </div>
+                </div>
+
+
+                <div className={styles.modal_divider}></div>
+
+
+                <div className={styles.modal_content}>
+                    {/* Colocar o corpo do modal aqui */}
+                </div>
+
+                <div className={styles.modal_actions}>
+                    Colocar os botões aqui
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Modal
--- src/screens/playground/PlaygroundScreen.tsx
+++ src/screens/playground/PlaygroundScreen.tsx
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from "react";
+import ModalTrilhas from "../../components/ModalTrilhas/ModalTrilhas";
 import CardCourse from "../CoursesScreen/Components/CardCourse";
 import styles from "./Playground.module.css";
+import CloseIcon from '@mui/icons-material/Close';
+
 
 function PlaygroundScreen() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
   return (
     <div className={styles.playground_container}>
       <div className={styles.playground_container_inner}>
-        <h1>Manter essa tela Limpa, após criar o componente</h1>
+        <button onClick={() => setModalIsOpen(true)}>Modal</button>
+        {modalIsOpen &&
+          <ModalTrilhas iconClose={<CloseIcon />} onClose={() => setModalIsOpen(false)} title="Tem certeza que deseja excluir?" />
+        }
       </div>
+
     </div>
   );
 }
