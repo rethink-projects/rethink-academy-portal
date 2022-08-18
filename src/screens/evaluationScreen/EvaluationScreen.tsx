0ca2a2f7b19cc6682ce2bf07c78104c19921efe4
FelipeReggiane <felipe.reggiane@rethink.dev>
2022-08-18 13:49:23 -0300

    feature/EvaluationScreen estrutura imcompleta

--- src/screens/evaluationScreen/EvaluationScreen.tsx
+++ src/screens/evaluationScreen/EvaluationScreen.tsx
@@ -1,11 +1,134 @@
-import React from "react";
+import React, { useState } from "react";
 import Breadcrumb from "../../components/Breadcrumb/Breadcrumb";
 import Dropdown from "../../components/Dropdown/Dropdown";
 import { useAuth } from "../../context/AuthContext";
 import styles from "./EvaluationScreen.module.css";
 
+type evaluationUserType = {
+  userName: string;
+  userSurname: string;
+  role: string;
+  month: string;
+  skills: string;
+  evaluation: evaluationType[];
+}
+
+type evaluationType ={
+  title: string;
+  grade: number;
+}
+
 const EvaluationScreen = () => {
   const { user } = useAuth();
+
+  const evaluationsData = [
+    {
+      userName: "Felipe",
+      userSurname: "Reggiane",
+      role: "Engenharia",
+      month: "março",
+      skills: "hardSkills",
+      evaluation : 
+      [
+        {
+          title: "Produto",
+          grade: 5,
+        },
+        {
+          title: "Produto",
+          grade: 5,
+        },
+        {
+          title: "Produto",
+          grade: 5,
+        },
+        {
+          title: "Produto",
+          grade: 5,
+        },
+        {
+          title: "Produto",
+          grade: 5,
+        },
+        {
+          title: "Produto",
+          grade: 5,
+        },
+      ]
+
+    },
+    {
+      userName: "Marcela",
+      userSurname: "Monteiro",
+      role: "Produto",
+      month: "abril",
+      skills: "hardSkills",
+      evaluation : 
+      [
+        {
+          title: "Produto",
+          grade: 5,
+        },
+        {
+          title: "Produto",
+          grade: 5,
+        },
+        {
+          title: "Produto",
+          grade: 5,
+        },
+        {
+          title: "Produto",
+          grade: 5,
+        },
+        {
+          title: "Produto",
+          grade: 5,
+        },
+        {
+          title: "Produto",
+          grade: 5,
+        },
+      ]
+
+    },
+    {
+      userName: "Pedro",
+      userSurname: "Lucas",
+      role: "Design",
+      month: "maio",
+      skills: "hardSkills",
+      evaluation : 
+      [
+        {
+          title: "Produto",
+          grade: 5,
+        },
+        {
+          title: "Produto",
+          grade: 5,
+        },
+        {
+          title: "Produto",
+          grade: 5,
+        },
+        {
+          title: "Produto",
+          grade: 5,
+        },
+        {
+          title: "Produto",
+          grade: 5,
+        },
+        {
+          title: "Produto",
+          grade: 5,
+        },
+      ]
+    },
+  ]
+
+  const [evaluations, setEvaluations] = useState<evaluationUserType[]>(evaluationsData);
 
   //   if (user.role != embassador) {
   //     return <div>Não tem permissão para acessar esta pagina</div>;
@@ -30,7 +153,7 @@ const EvaluationScreen = () => {
           <p>Aqui você consegue postar e editar as avaliações mensais de cada estagiário.</p>
         </div>
         <div className={styles.evaluationScreen_dropdown}>
-          <Dropdown setValue={()=>{}}  options={["março","abril","maio"]} id={"1"} initialText={"Mês"}/>
+          <Dropdown setValue={()=>{}}  options={["março","abril","maio","junho"]} id={"1"} initialText={"Mês"}/>
         </div>
       </div>
 
