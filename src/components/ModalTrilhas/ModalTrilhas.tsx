79a32dd0c992cdd07ca4092cdabaab0900737a1d
anaclara-rethink <102759771+anaclara-rethink@users.noreply.github.com>
2022-08-18 12:59:41 -0300

    restaurando playground

--- src/components/ModalTrilhas/ModalTrilhas.tsx
+++ src/components/ModalTrilhas/ModalTrilhas.tsx
@@ -1,55 +1,65 @@
-import React, { useState } from 'react'
-import IconButton from '../IconButton/IconButton';
-import styles from "./ModalTrilhas.module.css"
-import Checkbox from '../Checkbox/Checkbox';
-import { style } from '@mui/system';
-import SimpleButton from '../SimpleButton/SimpleButton';
+import React, { useState } from "react";
+import IconButton from "../IconButton/IconButton";
+import styles from "./ModalTrilhas.module.css";
+import CloseIcon from "@mui/icons-material/Close";
+import Checkbox from "../Checkbox/Checkbox";
+import { style } from "@mui/system";
+import SimpleButton from "../SimpleButton/SimpleButton";
 
 type modalProps = {
-    title: string;
-    iconClose?: JSX.Element;
-    children?: React.ReactNode;
-    onClose: VoidFunction;
-    id?: string;
-    onClickConfirm?: VoidFunction;
-    onClickCancel?: VoidFunction;
-    oneButton?: boolean;
-}
+  title: string;
+  iconClose?: JSX.Element;
+  children?: React.ReactNode;
+  onClose: VoidFunction;
+  id?: string;
+  onClickConfirm?: VoidFunction;
+  onClickCancel?: VoidFunction;
+  oneButton?: boolean;
+};
 
-const Modal = ({ oneButton = false, children, iconClose, title, onClose = () => { }, id = "outside", onClickConfirm, onClickCancel }: modalProps) => {
-
-    const handleOutsideClick = (e: any) => {
-        if (e.target.id === id) {
-            onClose();
-        }
+const ModalTrails = ({
+  oneButton = false,
+  children,
+  iconClose,
+  title,
+  onClose = () => {},
+  id = "outside",
+  onClickConfirm,
+  onClickCancel,
+}: modalProps) => {
+  const handleOutsideClick = (e: any) => {
+    if (e.target.id === id) {
+      onClose();
     }
+  };
 
-    return (
-        <div id={id} className={styles.modal_outside} onClick={handleOutsideClick}>
-            <div className={styles.modal_container}>
-                <div className={styles.modal_header} >
-                    <h1 className={styles.modal_title} >{title}</h1>
-                    <div onClick={onClose} className={styles.modal_header_icon}>
-                        {iconClose}
-                    </div>
-                </div>
-
-
-                <div className={styles.modal_divider}></div>
-
-
-                <div className={styles.modal_content}>
-                    {children}
-                </div>
-
-                <div className={styles.modal_actions}>
-                    {!oneButton &&
-                        <SimpleButton type="outline" text='Cancelar' onClick={onClickCancel!} />}
-                    <SimpleButton text='Confirmar' onClick={onClickConfirm!} />
-                </div>
-            </div>
+  return (
+    <div id={id} className={styles.modal_outside} onClick={handleOutsideClick}>
+      <div className={styles.modal_container}>
+        <div className={styles.modal_header}>
+          <h1 className={styles.modal_title}>{title}</h1>
+          <div onClick={onClose} className={styles.modal_header_icon}>
+            {iconClose}
+          </div>
         </div>
-    )
-}
 
-export default Modal
+        <div className={styles.modal_divider}></div>
+
+        <div className={styles.modal_content}>{children}</div>
+
+        <div className={styles.modal_actions}>
+          {!oneButton && (
+            <SimpleButton
+              type="outline"
+              text="Cancelar"
+              onClick={onClickCancel!}
+            />
+          )}
+          <SimpleButton text="Confirmar" onClick={onClickConfirm!} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ModalTrails;
--- src/screens/playground/PlaygroundScreen.tsx
+++ src/screens/playground/PlaygroundScreen.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./Playground.module.css";
+import ModalTrails from "../../components/ModalTrilhas/ModalTrilhas";
+import CloseIcon from "@mui/icons-material/Close";
 
 const PlaygroundScreen = () => {
-  return <div>PlaygroundScreen</div>;
+  const [modalIsOpen, setModalIsOpen] = useState(true);
+
+  return (
+    <div>
+      <div className={styles.playground_container}>
+        <div className={styles.playground_container_inner}>
+          <button onClick={() => setModalIsOpen(true)}>Modal</button>
+          {modalIsOpen && (
+            <ModalTrails
+              iconClose={<CloseIcon />}
+              onClose={() => setModalIsOpen(false)}
+              title="Tem certeza que deseja excluir?"
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PlaygroundScreen;
