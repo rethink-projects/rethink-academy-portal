f53a6e4bd3f2033f72fcad9c57e48a388bf71092
fabianakamo <fabiana.kamo@rethink.dev>
2022-08-18 16:37:59 -0300

    limpando playground

--- src/screens/playground/PlaygroundScreen.tsx
+++ src/screens/playground/PlaygroundScreen.tsx
@@ -1,4 +1,3 @@
-import AmbassadorViewTasksMM from "../../components/AmbassadorViewTasksMM/AmbassadorViewTasksMM";
 import styles from "./Playground.module.css";
 
 function PlaygroundScreen() {
@@ -6,10 +5,6 @@ function PlaygroundScreen() {
     <div className={styles.playground_container}>
       <div className={styles.main_content}>
         <h1>Manter essa tela Limpa, após criar o componente</h1>
-        <br />
-        <br />
-        <br />
-        <AmbassadorViewTasksMM />
       </div>
     </div>
   );
