aecc1ecf32557b9325033a945fd0c33f3520cd84
anaclara-rethink <102759771+anaclara-rethink@users.noreply.github.com>
2022-08-24 10:02:24 -0300

    Adicionando a função handlerOnChange

--- src/screens/CoursesScreen/Components/CardAddCourse/CardAddCourse.tsx
+++ src/screens/CoursesScreen/Components/CardAddCourse/CardAddCourse.tsx
@@ -13,7 +13,6 @@ type addCourseProps = {
 const CardAddCourse = ({ title, onClose = () => {} }: addCourseProps) => {
   const [currentStep, setCurrentStep] = useState(1);
   const [previousStep, setPreviousStep] = useState(1);
-  const [titleteste, setTitle] = useState("titleteste");
 
   const [formData, setFormData] = useState({
     name: "",
@@ -26,28 +25,40 @@ const CardAddCourse = ({ title, onClose = () => {} }: addCourseProps) => {
     level: "",
     workload: "",
     learn: "",
-    skils: "",
+    skills: "",
   });
+
+  const handlerOnChange = (event: any, key: string) => {
+    setFormData((prevValue) => {
+      return { ...prevValue, [key]: event.target.value };
+    });
+  };
 
   // console.log("anterior: " + previousStep + "\natual: " + currentStep);
 
   // console.log({ nomeDoCurso: formData.name, oferecidoPor: formData.offeredBy, nomeDoInstrutor: formData.nameInstructor });
-  console.log(formData);
+  // console.log(formData);
 
-  // console.log(
-  //   "nome do curso: " +
-  //     formData.name +
-  //     "\noferecido por: " +
-  //     formData.offeredBy +
-  //     "\nnome do instrutor: " +
-  //     formData.nameInstructor +
-  //     "\ndescrição do instrutor: " +
-  //     formData.descriptionInstructor +
-  //     "\nfoto do intrutor: " +
-  //     formData.avatar +
-  //     "\ncarga horaria: " +
-  //     formData.workload
-  // );
+  console.log(
+    "nome do curso: " +
+      formData.name +
+      "\noferecido por: " +
+      formData.offeredBy +
+      "\ndescrição do curso: " +
+      formData.description +
+      "\nnome do instrutor: " +
+      formData.nameInstructor +
+      "\ndescrição do instrutor: " +
+      formData.descriptionInstructor +
+      "\nfoto do intrutor: " +
+      formData.avatar +
+      "\ncarga horaria: " +
+      formData.workload +
+      "\nOque vc irá aprender: " +
+      formData.learn +
+      "\nHabilidades: " +
+      formData.skills
+  );
 
   return (
     <TrailModal
@@ -61,24 +72,6 @@ const CardAddCourse = ({ title, onClose = () => {} }: addCourseProps) => {
         currentStep === 1 ? "Cancelar" : currentStep === 4 ? "Sim" : "Voltar"
       }
       onClickConfirm={
-        // currentStep === 3
-        //   ? () => {
-        //       onClose();
-        //       console.log("concluido");
-        //     }
-        //   : () => setCurrentStep(currentStep + 1)
-
-        // currentStep === 3
-        //   ? () => {
-        //       onClose();
-        //       console.log("concluido");
-        //     }
-        //   : currentStep === 4
-        //   ? () => setCurrentStep(previousStep)
-        //   : () => {
-        //       setCurrentStep(currentStep + 1);
-        //       setPreviousStep(currentStep);
-        //     }
         currentStep === 4
           ? () => setCurrentStep(previousStep)
           : currentStep === 3
@@ -110,182 +103,159 @@ const CardAddCourse = ({ title, onClose = () => {} }: addCourseProps) => {
         {currentStep != 4 && (
           <p className={styles.currentStep}>Etapa {currentStep} de 3</p>
         )}
-        {/* Primeira linha */}
-        {currentStep === 3 ? (
-          <div className={`${styles.field} ${styles.field_2}`}>
+
+        {currentStep === 1 && (
+          <>
             <div className={styles.field}>
-              <p className={styles.title_field}>Nível do Curso</p>
-              <Dropdown
-                setValue={function (value: string): void {
-                  throw new Error("Function not implemented.");
-                }}
-                options={["Iniciante", "Intermediário", "Avançado"]}
-                id={"1"}
-                width={264}
-              />
-            </div>
-            <div className={styles.field}>
-              <p className={styles.title_field}>Carga Horária</p>
+              <p className={styles.title_field}>Nome do Curso</p>
               <InputText
                 type={"block"}
-                placeholder={"Exemplo: 30 horas"}
+                placeholder={"Exemplo: UX Writing"}
                 hasIcon={false}
                 nameInput={""}
-                value={formData.workload}
-                onChange={(e) =>
-                  setFormData((prevValue) => {
-                    return { ...prevValue, workload: e.target.value };
-                  })
+                value={formData.name}
+                onChange={(e) => handlerOnChange(e, "name")}
+              />
+            </div>
+            <div className={`${styles.field} ${styles.field_2}`}>
+              <div className={styles.field}>
+                <p className={styles.title_field}>Tipo de Conteúdo:</p>
+                <Dropdown
+                  setValue={function (value: string): void {
+                    throw new Error("Function not implemented.");
+                  }}
+                  options={["Curso", "Workshop", "Treinamento", "Palestra"]}
+                  id={"1"}
+                  width={264}
+                />
+              </div>
+              <div className={styles.field}>
+                <p className={styles.title_field}>Oferecido por:</p>
+                <InputText
+                  type={"block"}
+                  placeholder={"Rethink Academy"}
+                  hasIcon={false}
+                  nameInput={""}
+                  value={formData.offeredBy}
+                  onChange={(e) => handlerOnChange(e, "offeredBy")}
+                />
+              </div>
+            </div>
+            <div className={styles.field}>
+              <p className={styles.title_field}>Descrição do Curso</p>
+              <Textarea
+                type="block"
+                placeholder={
+                  "Adicione aqui um texto que sintetize o que é e quais os objetivos de aprendizado deste Curso..."
                 }
+                value={formData.description}
+                onChangetext={(e) => handlerOnChange(e, "description")}
               />
             </div>
-          </div>
-        ) : currentStep != 4 ? (
-          <div className={styles.field}>
-            <p className={styles.title_field}>
-              {currentStep === 1 ? "Nome do Curso" : "Nome do Instrutor"}
-            </p>
-            <InputText
-              type={"block"}
-              placeholder={
-                currentStep === 1
-                  ? "Exemplo: UX Writing"
-                  : "Exemplo: Débora Line"
-              }
-              hasIcon={false}
-              nameInput={""}
-              value={
-                currentStep === 1 ? formData.name : formData.nameInstructor
-              }
-              onChange={
-                currentStep === 1
-                  ? (e) =>
-                      setFormData((prevValue) => {
-                        return { ...prevValue, name: e.target.value };
-                      })
-                  : (e) =>
-                      setFormData((prevValue) => {
-                        return { ...prevValue, nameInstructor: e.target.value };
-                      })
-              }
-            />
-          </div>
-        ) : (
-          ""
+          </>
         )}
-        {/* Segunda linha */}
-        {currentStep === 1 ? (
-          // primeiro modal
-          <div className={`${styles.field} ${styles.field_2}`}>
+        {currentStep === 2 && (
+          <>
             <div className={styles.field}>
-              <p className={styles.title_field}>Tipo de Conteúdo:</p>
-              <Dropdown
-                setValue={function (value: string): void {
-                  throw new Error("Function not implemented.");
-                }}
-                options={["Curso", "Workshop", "Treinamento", "Palestra"]}
-                id={"1"}
-                width={264}
-              />
-            </div>
-            <div className={styles.field}>
-              <p className={styles.title_field}>Oferecido por:</p>
+              <p className={styles.title_field}>Nome do Instrutor</p>
               <InputText
                 type={"block"}
-                placeholder={"Rethink Academy"}
+                placeholder={"Exemplo: Débora Line"}
                 hasIcon={false}
                 nameInput={""}
-                value={formData.offeredBy}
-                onChange={(e) =>
-                  setFormData((prevValue) => {
-                    return { ...prevValue, offeredBy: e.target.value };
-                  })
-                }
+                value={formData.nameInstructor}
+                onChange={(e) => handlerOnChange(e, "nameInstructor")}
               />
             </div>
-          </div>
-        ) : currentStep === 2 ? (
-          // segundo modal
-          <div className={styles.field}>
-            <p className={styles.title_field}>{"Descrição do Instrutor"}</p>
-            <InputText
-              type={"block"}
-              placeholder={
-                "Descreva brevemente a área de atuação do instrutor..."
-              }
-              hasIcon={false}
-              nameInput={""}
-              value={formData.descriptionInstructor}
-              onChange={(e) =>
-                setFormData((prevValue) => {
-                  return {
-                    ...prevValue,
-                    descriptionInstructor: e.target.value,
-                  };
-                })
-              }
-            />
-          </div>
-        ) : currentStep != 4 ? (
-          //terceiro modal
-          <div className={styles.field}>
-            {" "}
-            <p className={styles.title_field}>
-              O que o Rethinker irá aprender neste curso:
-            </p>
-            <Textarea
-              type="block"
-              placeholder={
-                "Exemplo: Descreva que metodologias ou conteúdos, de cunho teórico, serãm abordados e desenvolvidos neste curso."
-              }
-            />
-          </div>
-        ) : (
-          ""
-        )}
-        {/* Terceira linha */}
-        <div className={styles.field}>
-          <p className={currentStep != 4 ? styles.title_field : styles.obs}>
-            {currentStep === 1
-              ? "Descrição do Curso"
-              : currentStep === 2
-              ? "Foto do Instrutor"
-              : currentStep != 4
-              ? "Habilidades que o Rethinker terá:"
-              : "Ao confirmar essa ação, você não poderá recuperar esses dados."}
-          </p>
-          {currentStep === 2 ? (
-            // modal 2
-            <>
+            <div className={styles.field}>
+              <p className={styles.title_field}>{"Descrição do Instrutor"}</p>
+              <InputText
+                type={"block"}
+                placeholder={
+                  "Descreva brevemente a área de atuação do instrutor..."
+                }
+                hasIcon={false}
+                nameInput={""}
+                value={formData.descriptionInstructor}
+                onChange={(e) => handlerOnChange(e, "descriptionInstructor")}
+              />
+            </div>
+            <div className={styles.field}>
+              <p className={styles.title_field}>Foto do Instrutor</p>
               <InputText
                 type={"block"}
                 placeholder={"https://Exemplo.com"}
                 hasIcon={false}
                 nameInput={""}
                 value={formData.avatar}
-                onChange={(e) =>
-                  setFormData((prevValue) => {
-                    return { ...prevValue, avatar: e.target.value };
-                  })
-                }
+                onChange={(e) => handlerOnChange(e, "avatar")}
               />
               <p className={styles.obs}>
                 Adicione o link para utilizar a imagem que deseja utilizar
               </p>
-            </>
-          ) : currentStep != 4 ? (
-            <Textarea
-              type="block"
-              placeholder={
-                currentStep === 1
-                  ? "Adicione aqui um texto que sintetize o que é e quais os objetivos de aprendizado deste Curso..."
-                  : "Exemplo: Descreva que conteúdos de teor prático, entregáveis ou skills que serão desenvolvidas no decorrer deste curso:"
-              }
-            />
-          ) : (
-            ""
-          )}
-        </div>
+            </div>
+          </>
+        )}
+
+        {currentStep === 3 && (
+          <>
+            <div className={`${styles.field} ${styles.field_2}`}>
+              <div className={styles.field}>
+                <p className={styles.title_field}>Nível do Curso</p>
+                <Dropdown
+                  setValue={function (value: string): void {
+                    throw new Error("Function not implemented.");
+                  }}
+                  options={["Iniciante", "Intermediário", "Avançado"]}
+                  id={"1"}
+                  width={264}
+                />
+              </div>
+              <div className={styles.field}>
+                <p className={styles.title_field}>Carga Horária</p>
+                <InputText
+                  type={"block"}
+                  placeholder={"Exemplo: 30 horas"}
+                  hasIcon={false}
+                  nameInput={""}
+                  value={formData.workload}
+                  onChange={(e) => handlerOnChange(e, "workload")}
+                />
+              </div>
+            </div>
+            <div className={styles.field}>
+              <p className={styles.title_field}>
+                O que o Rethinker irá aprender neste curso:
+              </p>
+              <Textarea
+                type="block"
+                placeholder={
+                  "Exemplo: Descreva que metodologias ou conteúdos, de cunho teórico, serãm abordados e desenvolvidos neste curso."
+                }
+                value={formData.learn}
+                onChangetext={(e) => handlerOnChange(e, "learn")}
+              />
+            </div>
+            <div className={styles.field}>
+              <p className={styles.title_field}>
+                Habilidades que o Rethinker terá:
+              </p>
+              <Textarea
+                type="block"
+                placeholder={
+                  "Exemplo: Descreva que conteúdos de teor prático, entregáveis ou skills que serão desenvolvidas no decorrer deste curso:"
+                }
+                value={formData.skills}
+                onChangetext={(e) => handlerOnChange(e, "skills")}
+              />
+            </div>
+          </>
+        )}
+        {currentStep === 4 && (
+          <p className={styles.currentStep}>
+            Ao confirmar essa ação, você não poderá recuperar esses dados.
+          </p>
+        )}
       </div>
     </TrailModal>
   );
