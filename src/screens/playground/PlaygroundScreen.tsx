f93fd92839fb7326ca802e88a96afa0119da9f8f
Lucaspaula6 <lucas.paula@rethink.dev>
2022-08-24 11:36:39 -0300

    Alterações incremntais

--- src/screens/playground/PlaygroundScreen.tsx
+++ src/screens/playground/PlaygroundScreen.tsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
+import Dropdown from "../../components/Dropdown/Dropdown";
 import styles from "./Playground.module.css";
 
 function PlaygroundScreen() {
+  const [value, setValue] = useState("");
   return (
     <div className={styles.playground_container}>
       <div className={styles.main_content}>
         <h1>Manter essa tela Limpa, após criar o componente</h1>
+        <Dropdown
+          options={["asd", "as"]}
+          id={"wd"}
+          setValue={setValue}
+          value={value}
+          width={190}
+        />
       </div>
     </div>
   );
