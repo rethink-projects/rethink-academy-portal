eca45111de5e1e30f83f9574bdd568644ad03f88
Lucaspaula6 <lucas.paula@rethink.dev>
2022-08-29 15:27:24 -0300

    Feature: EmptyModal
    Criação do modal vazio

---
+++ src/components/EmptyModal/EmptyModal.tsx
@@ -0,0 +1,24 @@
+import styles from "./EmptyModal.module.css";
+import SimpleButton from "../SimpleButton/SimpleButton";
+import CloseIcon from "@mui/icons-material/Close";
+
+type modalProps = {
+  children?: React.ReactNode;
+  onClose: VoidFunction;
+  id: string;
+};
+
+const EmptyModal = ({ children, onClose, id }: modalProps) => {
+  const handleOutsideClick = (e: any) => {
+    if (e.target.id === id) {
+      onClose();
+    }
+  };
+  return (
+    <div id={id} className={styles.modal_outside} onClick={handleOutsideClick}>
+      <div className={styles.modal_container}>{children}</div>
+    </div>
+  );
+};
+
+export default EmptyModal;
--- src/screens/playground/PlaygroundScreen.tsx
+++ src/screens/playground/PlaygroundScreen.tsx
@@ -1,10 +1,20 @@
+import EmptyModal from "../../components/EmptyModal/EmptyModal";
 import styles from "./Playground.module.css";
+import { useState } from "react";
 
 function PlaygroundScreen() {
+  const [seeModal, setSeeModal] = useState(false);
+
   return (
     <div className={styles.playground_container}>
       <div className={styles.playground_container_inner}>
         <h1>Manter essa tela Limpa, após criar o componente</h1>
+        <button onClick={() => setSeeModal(true)}>Teste</button>
+        {seeModal && (
+          <EmptyModal onClose={() => setSeeModal(!seeModal)} id={"s"}>
+            <div style={{ width: "500px", height: "500px" }}>desasd</div>
+          </EmptyModal>
+        )}
       </div>
     </div>
   );
