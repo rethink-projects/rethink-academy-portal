02a5da439254d0080176de36aaefa88682c05069
gabsrethink <gabriel.melo@rethink.dev>
2022-08-29 09:32:41 -0300

    page boilerplate

---
+++ src/screens/PersonalDevelpment/PersonalDevelopmentScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./PersonalDevelopmentScreen.module.css";
+
+const PersonalDevelopmentScreen = () => {
+  return (
+    <div className={styles.graph_container}>
+      <div>
+        <h1>Evolução de Habilidades</h1>
+      </div>
+    </div>
+  );
+};
+
+export default PersonalDevelopmentScreen;
