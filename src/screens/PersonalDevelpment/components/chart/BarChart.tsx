4283513a787e3ab78ac3fd9492ddc9e5c25b3931
gabsrethink <gabriel.melo@rethink.dev>
2022-09-02 15:24:21 -0300

    gráfico mudando dados

--- src/screens/PersonalDevelpment/components/chart/BarChart.tsx
+++ src/screens/PersonalDevelpment/components/chart/BarChart.tsx
@@ -1,9 +1,8 @@
-import React, { PureComponent } from "react";
-import styles from "./BarChart.module.css"
+import React, { PureComponent, useState } from "react";
+import styles from "./BarChart.module.css";
 import {
   BarChart,
   Bar,
-  Cell,
   XAxis,
   YAxis,
   CartesianGrid,
@@ -11,70 +10,136 @@ import {
   Legend,
   ResponsiveContainer,
 } from "recharts";
+import axios from "axios";
 
-const data = [
+export const data = [
   {
     name: "Jun/21",
-    Pesquisa: 2,
+    skill: 2,
     pv: 0,
   },
   {
     name: "Jul/21",
-    Pesquisa: 3,
+    skill: 3,
     pv: 0,
   },
   {
     name: "Ago/21",
-    Pesquisa: 3,
+    skill: 3,
     pv: 0,
   },
   {
     name: "Set/21",
-    Pesquisa: 4,
+    skill: 4,
     pv: 0,
   },
   {
     name: "Out/21",
-    Pesquisa: 4,
+    skill: 4,
     pv: 0,
   },
   {
     name: "Nov/21",
-    Pesquisa: 4,
+    skill: 4,
     pv: 0,
   },
   {
     name: "Dez/21",
-    Pesquisa: 4,
+    skill: 4,
     pv: 0,
   },
   {
     name: "Jan/22",
-    Pesquisa: 5,
+    skill: 5,
     pv: 0,
   },
   {
     name: "Fev/22",
-    Pesquisa: 0,
+    skill: 0,
     pv: 1,
   },
   {
     name: "Mar/22",
-    Pesquisa: 0,
+    skill: 0,
     pv: 1,
   },
   {
     name: "Abr/22",
-    Pesquisa: 0,
+    skill: 0,
     pv: 1,
   },
   {
     name: "Mai/22",
-    Pesquisa: 0,
+    skill: 0,
     pv: 1,
   },
 ];
 
+export const dataDois = [
+  {
+    name: "Jun/21",
+    skill: 3,
+    pv: 0,
+  },
+  {
+    name: "Jul/21",
+    skill: 4,
+    pv: 0,
+  },
+  {
+    name: "Ago/21",
+    skill: 4,
+    pv: 0,
+  },
+  {
+    name: "Set/21",
+    skill: 4,
+    pv: 0,
+  },
+  {
+    name: "Out/21",
+    skill: 4,
+    pv: 0,
+  },
+  {
+    name: "Nov/21",
+    skill: 4,
+    pv: 0,
+  },
+  {
+    name: "Dez/21",
+    skill: 4,
+    pv: 0,
+  },
+  {
+    name: "Jan/22",
+    skill: 5,
+    pv: 0,
+  },
+  {
+    name: "Fev/22",
+    skill: 0,
+    pv: 1,
+  },
+  {
+    name: "Mar/22",
+    skill: 0,
+    pv: 1,
+  },
+  {
+    name: "Abr/22",
+    skill: 0,
+    pv: 1,
+  },
+  {
+    name: "Mai/22",
+    skill: 0,
+    pv: 1,
+  },
+];
+
+/* const {data} = axios.get("url")
+const {data} = axios.get("url", {query: {skill: "skill1"} }) */
 const CustomTooltip = ({ active, payload, label }: any) => {
   if (active && payload && payload.length) {
     return (
@@ -88,43 +153,41 @@ const CustomTooltip = ({ active, payload, label }: any) => {
   return null;
 };
 
-export default class Example extends PureComponent {
-  static demoUrl = "https://codesandbox.io/s/stacked-bar-chart-s47i2";
+const CustomBarChart = ({ graphData }: { graphData: any }) => {
+  return (
+    <ResponsiveContainer width="100%" height="65%">
+      <BarChart
+        width={500}
+        height={200}
+        data={graphData}
+        margin={{
+          top: 20,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid vertical={false} />
+        <XAxis dataKey="name" />
+        <YAxis type="number" domain={[0, 5]} tickCount={6} />
+        <Tooltip cursor={false} content={CustomTooltip} />
+        <Bar
+          dataKey="skill"
+          stackId="a"
+          fill="#82A8F8"
+          barSize={40}
+          radius={[4, 4, 0, 0]}
+        />
+        <Bar
+          dataKey="pv"
+          stackId="a"
+          fill="#D4D4D4"
+          barSize={40}
+          radius={[4, 4, 0, 0]}
+        />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
 
-  render() {
-    return (
-      <ResponsiveContainer width="100%" height="65%">
-        <BarChart
-          width={500}
-          height={200}
-          data={data}
-          margin={{
-            top: 20,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid vertical={false} />
-          <XAxis dataKey="name" />
-          <YAxis type="number" domain={[0, 5]} tickCount={6} />
-          <Tooltip cursor={false} content={CustomTooltip} />
-          <Bar
-            dataKey="Pesquisa"
-            stackId="a"
-            fill="#82A8F8"
-            barSize={40}
-            radius={[4, 4, 0, 0]}
-          />
-          <Bar
-            dataKey="pv"
-            stackId="a"
-            fill="#D4D4D4"
-            barSize={40}
-            radius={[4, 4, 0, 0]}
-          />
-        </BarChart>
-      </ResponsiveContainer>
-    );
-  }
-}
+export default CustomBarChart;
