91243459db4522ebf21a6263f541a3d87b283255
amandadclsRethink <amanda.duarte@rethink.dev>
2022-09-08 20:26:42 -0300

    Goals Card - Link to BackEnd

--- src/components/CardMetas/CardMetas.tsx
+++ src/components/CardMetas/CardMetas.tsx
@@ -26,60 +26,43 @@ type goalsData = {
   goalListId: string;
 };
 
-// const goalsDataSim = [
-//   {
-//     id: 1,
-//     month: "Janeiro",
-//     isActive: true,
-//     goalsData: [
-//       { title: "meta 1", conclude: true, id: 1 },
-//       { title: "meta 2", conclude: false, id: 2 },
-//       { title: "meta 3", conclude: true, id: 3 },
-//       { title: "meta 4", conclude: false, id: 4 },
-//       { title: "meta 5", conclude: true, id: 5 },
-//       { title: "meta 6", conclude: false, id: 6 },
-//       { title: "meta 7", conclude: false, id: 7 },
-//       { title: "meta 8", conclude: true, id: 8 },
-//       { title: "meta 9", conclude: false, id: 9 },
-//       { title: "meta 10", conclude: false, id: 10 },
-//       { title: "meta 11", conclude: true, id: 11 },
-//       { title: "mes errado", conclude: true, id: 12 },
-//       {
-//         title: "mes certo, escrita diferente ",
-//         conclude: false,
-//         id: 13,
-//       },
-//     ],
-//   },
-//   {
-//     id: 2,
-//     month: "Outubro",
-//     isActive: false,
-//     goalsData: [
-//       { id: 1, title: "asdfg", conclude: true },
-//       { id: 2, title: "qwerty", conclude: false },
-//     ],
-//   },
-// ];
+type GoalsCardProps = {
+  studentEmail?: string;
+};
 
-const CardMetas = () => {
+const CardMetas = ({ studentEmail }: GoalsCardProps) => {
   const { user } = useAuth();
 
-  const [goals, setGoals] = useState<goalList>();
-  // const [goals, setGoals] = useState<any>();
+  const [goalList, setGoalList] = useState<goalList>();
+  const [goals, setGoals] = useState<any>();
+  const [update, setUpdate] = useState(false);
   const [isModalOpen, setModalOpen] = useState(false);
 
   // console.log(goalList);
-  console.log(goals);
+  // console.log(goals);
+  // console.log(studentEmail);
 
-  const getGoals = async () => {
+  const getGoalList = async () => {
     if (user?.role === "STUDENT") {
       try {
         const userGoalsList = await axios.get(
           `http://localhost:4000/api/goalList/${user.email}`
         );
-        setGoals(userGoalsList.data[userGoalsList.data.length - 1]);
-        // setGoals(userGoalsList.data[userGoalsList.data.length - 1].goal);
+        setGoalList(userGoalsList.data[userGoalsList.data.length - 1]);
+        setGoals(userGoalsList.data[userGoalsList.data.length - 1].goal);
+        return;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    if (user?.role === "EMBASSADOR") {
+      try {
+        const studentGoalsList = await axios.get(
+          `http://localhost:4000/api/goalList/${studentEmail}`
+        );
+
+        setGoalList(studentGoalsList.data[studentGoalsList.data.length - 1]);
+        setGoals(studentGoalsList.data[studentGoalsList.data.length - 1].goal);
         return;
       } catch (error) {
         console.log(error);
@@ -88,8 +71,8 @@ const CardMetas = () => {
   };
 
   useEffect(() => {
-    getGoals();
-  }, [user]);
+    getGoalList();
+  }, [user, update]);
 
   const updateGoal = async (
     id: string,
@@ -97,42 +80,42 @@ const CardMetas = () => {
     title?: string,
     goalsListId?: string
   ) => {
-    // try {
-    //   const goalUpdate = await axios.patch(
-    //     `http://localhost:4000/api/goal/${id!}`,
-    //     { conclude }
-    //   );
-    //   const goalsData = await goals.map((goal: any) => {
-    //     if (goal.id === goalsListId) {
-    //       return {
-    //         ...goal,
-    //         isUpdateGoalMode: "",
-    //         goal: goal.goal.map((goalsIntern: any) => {
-    //           if (goalsIntern.id === id) {
-    //             return {
-    //               ...goalsIntern,
-    //               title,
-    //               conclude,
-    //             };
-    //           } else {
-    //             return {
-    //               ...goalsIntern,
-    //             };
-    //           }
-    //         }),
-    //       };
-    //     } else {
-    //       return {
-    //         ...goal,
-    //       };
-    //     }
-    // });
-    // setGoals(goalsData);
-    // setNewTitleGoal("");
-    return;
-    // } catch (error) {
-    //   console.log(error);
-    // }
+    try {
+      const goalUpdate = await axios.patch(
+        `http://localhost:4000/api/goal/${id!}`,
+        { conclude }
+      );
+      setUpdate((current) => !current);
+      const goalsData = await goals.map((goal: any) => {
+        if (goal.id === goalsListId) {
+          return {
+            ...goal,
+            isUpdateGoalMode: "",
+            goal: goal.goal.map((goalsIntern: any) => {
+              if (goalsIntern.id === id) {
+                return {
+                  ...goalsIntern,
+                  title,
+                  conclude,
+                };
+              } else {
+                return {
+                  ...goalsIntern,
+                };
+              }
+            }),
+          };
+        } else {
+          return {
+            ...goal,
+          };
+        }
+      });
+      setGoals(goalsData);
+      return;
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const handleClick = (
@@ -141,43 +124,39 @@ const CardMetas = () => {
     goalListId: string,
     props: any
   ) => {
-    console.log(props);
-    // updateGoal(id, props.props, title, goalListId);
-    // goals &&
-    //   setGoals(() =>
-    //     goals.map((goal: any) => {
-    //       if (goal.id === props.idGoal) {
-    //         return {
-    //           ...goal,
-    //           goal: goal.goal.map((goalsIntern: any) => {
-    //             if (goalsIntern.id === props.id) {
-    //               return {
-    //                 ...goalsIntern,
-    //                 conclude: props.props,
-    //               };
-    //             } else {
-    //               return {
-    //                 ...goalsIntern,
-    //               };
-    //             }
-    //           }),
-    //         };
-    //       } else {
-    //         return {
-    //           ...goal,
-    //         };
-    //       }
-    //     })
-    //   );
+    // console.log(props);
+    updateGoal(id, props.props, title, goalListId);
+    goals &&
+      setGoals(() =>
+        goals.map((goal: any) => {
+          if (goal.id === props.idGoal) {
+            return {
+              ...goal,
+              goal: goal.goal.map((goalsIntern: any) => {
+                if (goalsIntern.id === props.id) {
+                  return {
+                    ...goalsIntern,
+                    conclude: props.props,
+                  };
+                } else {
+                  return {
+                    ...goalsIntern,
+                  };
+                }
+              }),
+            };
+          } else {
+            return {
+              ...goal,
+            };
+          }
+        })
+      );
   };
 
   if (goals) {
-    let completedGoals = goals.goal.filter(
-      (goal: any) => goal.conclude === true
-    );
-    let uncompletedGoals = goals.goal.filter(
-      (goal: any) => goal.conclude === false
-    );
+    let completedGoals = goals.filter((goal: any) => goal.conclude === true);
+    let uncompletedGoals = goals.filter((goal: any) => goal.conclude === false);
 
     return (
       <div className={styles.CardUltimasMetas_info}>
@@ -185,13 +164,8 @@ const CardMetas = () => {
           <img src={Images.LastGoalsMedal} alt="medalha ultimas Goals" />
 
           <div className={styles.CardUltimasMetas_inner_info}>
-            <h1>{goals?.name}</h1>
-            <p>
-              {goals.goal.length < 10
-                ? `0${goals.goal.length}`
-                : goals.goal.length}{" "}
-              Metas
-            </p>
+            <h1>{goalList?.name}</h1>
+            <p>{goals.length < 10 ? `0${goals.length}` : goals.length} Metas</p>
           </div>
 
           <ArrowForwardIosRoundedIcon
@@ -210,9 +184,7 @@ const CardMetas = () => {
                   size="small"
                   isChecked={value.conclude}
                   setIsChecked={(props) =>
-                    handleClick(value.id, value.title, goals.goal[index].id, {
-                      props,
-                    })
+                    handleClick(value.id, value.title, goals.id, { props })
                   }
                   key={value.id}
                 />
@@ -227,9 +199,7 @@ const CardMetas = () => {
                   size="small"
                   isChecked={value.conclude}
                   setIsChecked={(props) =>
-                    handleClick(value.id, value.title, goals.goal[index].id, {
-                      props,
-                    })
+                    handleClick(value.id, value.title, goals.id, { props })
                   }
                   key={value.id}
                 />
@@ -241,12 +211,12 @@ const CardMetas = () => {
         <div className={styles.CardUltimasMetas_progress}>
           <div className={styles.progress_Goals}>
             <p>{completedGoals.length} </p>
-            <span> /{goals.goal.length} </span>
+            <span> /{goals.length} </span>
           </div>
 
           <ProgressBar
             width={362}
-            totalValue={goals.goal.length}
+            totalValue={goals.length}
             relativeValue={completedGoals.length}
           />
 
@@ -254,9 +224,18 @@ const CardMetas = () => {
         </div>
       </div>
     );
+  } else {
+    return (
+      <div className={styles.CardUltimasMetas_info}>
+        <div className={styles.noGoals}>
+          <div className={styles.noGoalsWarning}>
+            <img src={Images.infoNotes} alt="Ícone de informação" />
+            <p>Você ainda não possui nenhuma meta.</p>
+          </div>
+        </div>
+      </div>
+    );
   }
-
-  return <div>teste</div>;
 };
 
 export default CardMetas;
