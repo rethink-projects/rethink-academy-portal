146c108a8b467d46d47aa04f03f9613fbb019dcc
sthephanytezza-dev <sthephany.tezza@rethink.dev>
2022-09-08 19:08:46 -0300

    alteração calendário mostrar intervalo de data

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -1,11 +1,16 @@
-import { useState } from "react";
-import Calendar from "./Components/Calendar";
+import { useEffect, useState } from "react";
+import { DateRange } from "react-date-range";
+import pt from "date-fns/locale/pt";
 
 // CSS
 import styles from "./DatePicker.module.css";
 
 // Assets
 import Images from "../../assets";
+
+import "./Components/styles.css";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
 
 type DatePickerProps = {
   hasIcon?: boolean;
@@ -22,6 +27,45 @@ export const DatePicker = ({
 }: DatePickerProps) => {
   const [active, setActive] = useState(false);
 
+  const [state, setState] = useState<
+    { startDate?: Date; endDate?: Date; key?: string }[]
+  >([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
+
+  const formatDate = (date: string) => {
+    const helper = date.split("/");
+    return `${helper[0]}/${helper[1]}`;
+  };
+
+  const [dateInitial, setDateInitial] = useState(
+    formatDate(
+      state[0].startDate!.toLocaleDateString("pt-BR", { timeZone: "UTC" })
+    )
+  );
+  const [dateFinal, setDateFinal] = useState(
+    formatDate(
+      state[0].endDate!.toLocaleDateString("pt-BR", { timeZone: "UTC" })
+    )
+  );
+
+  useEffect(() => {
+    setDateInitial(
+      formatDate(
+        state[0].startDate!.toLocaleDateString("pt-BR", { timeZone: "UTC" })
+      )
+    );
+    setDateFinal(
+      formatDate(
+        state[0].endDate!.toLocaleDateString("pt-BR", { timeZone: "UTC" })
+      )
+    );
+  }, [state]);
+
   return (
     <div
       className={
@@ -36,7 +80,7 @@ export const DatePicker = ({
                 <img src={Images.icons.eyeIcon} alt="An eye icon" />
               </>
             )}
-            <p>{placeholder}</p>
+            <p>{`${dateInitial} à ${dateFinal}`}</p>
           </div>
           <div className={styles.datePicker_calendarIcon}>
             <img src={Images.icons.calendarIcon} alt="A calendar icon" />
@@ -45,7 +89,18 @@ export const DatePicker = ({
       </div>
       {active && (
         <div className={`${styles.datePicker_calendar} ${styles[position]}`}>
-          <Calendar />
+          <DateRange
+            className={"large"}
+            editableDateInputs={true}
+            onChange={(item) => {
+              setState([item.selection]);
+            }}
+            moveRangeOnFirstSelection={false}
+            ranges={state}
+            locale={pt}
+            showMonthAndYearPickers={false}
+            showDateDisplay={false}
+          />
         </div>
       )}
     </div>
