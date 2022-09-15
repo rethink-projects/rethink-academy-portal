aa46f136896b8ff475da3c92e2212531dd75b732
fabianakamo <fabiana.kamo@rethink.dev>
2022-09-15 20:07:47 -0300

    alterando cores do SingleCalendar

--- src/components/DatePicker/SingleDatePicker.tsx
+++ src/components/DatePicker/SingleDatePicker.tsx
@@ -5,6 +5,7 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import TextField from "@mui/material/TextField";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import styles from "./DatePicker.module.css";
+import "./styles.css";
 
 const SingleDatePicker = ({
   formDataValue,
@@ -42,3 +43,42 @@ const SingleDatePicker = ({
 };
 
 export default SingleDatePicker;
+
+// import React, { useEffect, useState } from "react";
+// import "./Components/styles.css";
+// import { DateRange } from "react-date-range";
+// import pt from "date-fns/locale/pt";
+
+// import "react-date-range/dist/styles.css";
+// import "react-date-range/dist/theme/default.css";
+
+// const CalendarComponent = () => {
+//   const date = new Date();
+
+//   const [state, setState] = useState<
+//     { startDate?: Date; endDate?: Date; key?: string }[]
+//   >([
+//     {
+//       startDate: new Date(date.valueOf() - date.getTimezoneOffset() * 60000), // PEGAR DATA ATUAL
+//       endDate: new Date(date.valueOf() - date.getTimezoneOffset() * 60000),
+//       key: "selection",
+//     },
+//   ]);
+
+//   return (
+//     <DateRange
+//       className={"large"}
+//       editableDateInputs={true}
+//       onChange={(item) => {
+//         setState([item.selection]);
+//       }}
+//       moveRangeOnFirstSelection={false}
+//       ranges={state}
+//       locale={pt}
+//       showMonthAndYearPickers={false}
+//       showDateDisplay={false}
+//     />
+//   );
+// };
+
+// export default CalendarComponent;
