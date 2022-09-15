494c72acd88cb0ad1e4d695905675df0f24a17ee
FelipeReggiane <felipe.reggiane@rethink.dev>
2022-09-15 19:52:17 -0300

    fix button notas header

--- src/screens/Layout/components/Header/Header.tsx
+++ src/screens/Layout/components/Header/Header.tsx
@@ -1,18 +1,26 @@
+import { Link } from "react-router-dom";
 import Images from "../../../../assets";
 import ButtonWithIcon from "../../../../components/ButtonWithIcon/ButtonWithIcon";
+import { useAuth } from "../../../../context/AuthContext";
 import styles from "./Header.module.css";
 
 function Header() {
+  const { user } = useAuth();
+
   return (
     <div className={styles.header_container}>
       <div className={styles.header_inner}>
-        <ButtonWithIcon
-          icon={<img src={Images.icons.IconEdit} alt='Notas' />}
-          position='left'
-          text='Notas'
-          size='small'
-          type='secondary'
-        />
+        <Link to="/dashboard/notas">
+          <ButtonWithIcon
+            icon={<img src={Images.icons.IconEdit} alt="Notas" />}
+            position="left"
+            text={
+              user && user.role === "AMBASSADOR" ? "Notas privadas" : "Notas"
+            }
+            size="block"
+            type="secondary"
+          />
+        </Link>
       </div>
     </div>
   );
