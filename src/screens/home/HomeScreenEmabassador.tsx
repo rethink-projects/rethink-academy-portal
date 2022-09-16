d1b057f6bda173fb7487795d2b630859c958e769
Gabriel Gomes <gabriel.gomes@rethink.dev>
2022-09-16 09:57:56 -0300

    fix homescreen background

--- src/screens/home/HomeScreenEmabassador.tsx
+++ src/screens/home/HomeScreenEmabassador.tsx
@@ -132,124 +132,126 @@ const HomeScreenEmabassador = ({ user }: any) => {
   ];
 
   return (
-    <div className={Styles.home_limited_container}>
-      <div className={Styles.home_container}>
-        <p className={Styles.home_hello}>E ai, {user.name} 🥳</p>
-        <div className={Styles.home_header}>
-          <div>
-            <p className={Styles.title}>Estagiários</p>
-            <p className={Styles.tip}>
-              Selecione uma turma para acompanhar a evolução
-            </p>
-          </div>
-          <Dropdown
-            setValue={setAcademyClass}
-            value={academyClass}
-            options={["1/2022"]}
-            id={"Team"}
-            width={185}
-          />
-        </div>
-
-        <div className={Styles.home_content}>
-          <div className={Styles.home_content_header}>
-            <p>
-              Selecione um estagiário para ter acesso aos seus dados de
-              desenvolvimento:
-            </p>
-            <div className={Styles.role_selector}>
-              <button
-                onClick={() => handlerOnClickRoleSelector("DESIGN")}
-                className={roleClass("DESIGN")}
-              >
-                Design
-              </button>
-              <button
-                onClick={() => handlerOnClickRoleSelector("ENGINEERING")}
-                className={roleClass("ENGINEERING")}
-              >
-                Engenharia
-              </button>
-              <button
-                onClick={() => handlerOnClickRoleSelector("PRODUCT")}
-                className={roleClass("PRODUCT")}
-              >
-                Produto
-              </button>
+    <div className={Styles.max_container}>
+      <div className={Styles.home_limited_container}>
+        <div className={Styles.home_container}>
+          <p className={Styles.home_hello}>E ai, {user.name} 🥳</p>
+          <div className={Styles.home_header}>
+            <div>
+              <p className={Styles.title}>Estagiários</p>
+              <p className={Styles.tip}>
+                Selecione uma turma para acompanhar a evolução
+              </p>
             </div>
-          </div>
-          <div className={Styles.cards_container}>
-            {usersFilterd.map((intern, index) => (
-              <div className={Styles.student_card} key={index}>
-                <div className={Styles.student_card_arrow} />
-                <div className={Styles.student_card_avatar}>
-                  <img
-                    src={intern.avatar}
-                    className={Styles.avatar_img}
-                    alt="Student Avatar Arrow"
-                  />
-                  <div className={Styles.avatar_level}>
-                    <img
-                      src={Images.icons.level_Icon}
-                      className={Styles.avatar_icon}
-                      alt="Rethink Arrow"
-                    />
-                    {user.level}
-                  </div>
-                </div>
-
-                <div className={Styles.student_card_text}>
-                  <div>{intern.name + " " + intern.surname}</div>
-                  <button className={Styles.card_role}>Engenharia</button>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className={Styles.progress_container}>
-          <p className={Styles.title}>Progresso da Turma</p>
-          <p className={Styles.tip}>
-            Confira em que ponto da jornada essa turma está!
-          </p>
-        </div>
-        <div className={Styles.progress_content}>
-          {progressInfo.map(
-            ({ totalValue, relativeValue, desc, title }, index) => (
-              <div className={Styles.progress_item} key={index}>
-                <ProgressBar
-                  color="dark"
-                  size="large"
-                  width={326}
-                  totalValue={totalValue}
-                  relativeValue={relativeValue}
-                />
-                <div className={Styles.progress_description}>
-                  <img
-                    src={Images.Emblem}
-                    alt=""
-                    style={{ width: "32px", height: "32px" }}
-                  />
-                  <div>
-                    <h1>{title}</h1>
-                    <p>{desc}</p>
-                  </div>
-                </div>
-              </div>
-            )
-          )}
-          <div className={Styles.progress_status}>
-            <img src={Images.ProgressBack} alt="Background" />
-            <img
-              src={end ? Images.ProgressEndIcon : Images.ProgressIcon}
-              alt="Background"
-              className={Styles.ProgressIcon}
+            <Dropdown
+              setValue={setAcademyClass}
+              value={academyClass}
+              options={["1/2022"]}
+              id={"Team"}
+              width={185}
             />
-            <div className={Styles.status_description}>
-              <p className={Styles.title}></p>
-              {end ? "Academy Concluído!" : "Academy em Progresso!"}
-              <p className={Styles.tip}>Programa de Estágio 2022</p>
+          </div>
+
+          <div className={Styles.home_content}>
+            <div className={Styles.home_content_header}>
+              <p>
+                Selecione um estagiário para ter acesso aos seus dados de
+                desenvolvimento:
+              </p>
+              <div className={Styles.role_selector}>
+                <button
+                  onClick={() => handlerOnClickRoleSelector("DESIGN")}
+                  className={roleClass("DESIGN")}
+                >
+                  Design
+                </button>
+                <button
+                  onClick={() => handlerOnClickRoleSelector("ENGINEERING")}
+                  className={roleClass("ENGINEERING")}
+                >
+                  Engenharia
+                </button>
+                <button
+                  onClick={() => handlerOnClickRoleSelector("PRODUCT")}
+                  className={roleClass("PRODUCT")}
+                >
+                  Produto
+                </button>
+              </div>
             </div>
-            <div className={Styles.progress_emoji}>💚 {end ? "🎉" : ""}</div>
+            <div className={Styles.cards_container}>
+              {usersFilterd.map((intern, index) => (
+                <div className={Styles.student_card} key={index}>
+                  <div className={Styles.student_card_arrow} />
+                  <div className={Styles.student_card_avatar}>
+                    <img
+                      src={intern.avatar}
+                      className={Styles.avatar_img}
+                      alt="Student Avatar Arrow"
+                    />
+                    <div className={Styles.avatar_level}>
+                      <img
+                        src={Images.icons.level_Icon}
+                        className={Styles.avatar_icon}
+                        alt="Rethink Arrow"
+                      />
+                      {user.level}
+                    </div>
+                  </div>
+
+                  <div className={Styles.student_card_text}>
+                    <div>{intern.name + " " + intern.surname}</div>
+                    <button className={Styles.card_role}>Engenharia</button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className={Styles.progress_container}>
+            <p className={Styles.title}>Progresso da Turma</p>
+            <p className={Styles.tip}>
+              Confira em que ponto da jornada essa turma está!
+            </p>
+          </div>
+          <div className={Styles.progress_content}>
+            {progressInfo.map(
+              ({ totalValue, relativeValue, desc, title }, index) => (
+                <div className={Styles.progress_item} key={index}>
+                  <ProgressBar
+                    color="dark"
+                    size="large"
+                    width={326}
+                    totalValue={totalValue}
+                    relativeValue={relativeValue}
+                  />
+                  <div className={Styles.progress_description}>
+                    <img
+                      src={Images.Emblem}
+                      alt=""
+                      style={{ width: "32px", height: "32px" }}
+                    />
+                    <div>
+                      <h1>{title}</h1>
+                      <p>{desc}</p>
+                    </div>
+                  </div>
+                </div>
+              )
+            )}
+            <div className={Styles.progress_status}>
+              <img src={Images.ProgressBack} alt="Background" />
+              <img
+                src={end ? Images.ProgressEndIcon : Images.ProgressIcon}
+                alt="Background"
+                className={Styles.ProgressIcon}
+              />
+              <div className={Styles.status_description}>
+                <p className={Styles.title}></p>
+                {end ? "Academy Concluído!" : "Academy em Progresso!"}
+                <p className={Styles.tip}>Programa de Estágio 2022</p>
+              </div>
+              <div className={Styles.progress_emoji}>💚 {end ? "🎉" : ""}</div>
+            </div>
           </div>
         </div>
       </div>
