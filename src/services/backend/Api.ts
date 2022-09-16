827bfb4a86db339f7d472d91da270ac794d1a918
Gabriel Gomes <gabriel.gomes@rethink.dev>
2022-09-16 11:25:41 -0300

    .env Backend URL

--- src/services/backend/Api.ts
+++ src/services/backend/Api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: process.env.BACKEND_URL,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
