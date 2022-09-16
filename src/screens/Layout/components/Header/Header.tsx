1bf676b7b77577f76f10f16be5ebb04ee0983bd4
gabsrethink <gabriel.melo@rethink.dev>
2022-09-16 14:41:58 -0300

    fix nas notas e alguns ajustes no css

--- src/screens/Layout/components/Header/Header.tsx
+++ src/screens/Layout/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Images from "../../../../assets";
 import ButtonWithIcon from "../../../../components/ButtonWithIcon/ButtonWithIcon";
 import { useAuth } from "../../../../context/AuthContext";
@@ -6,21 +6,22 @@ import styles from "./Header.module.css";
 
 function Header() {
   const { user } = useAuth();
+  const navigate = useNavigate();
 
   return (
     <div className={styles.header_container}>
       <div className={styles.header_inner}>
-        <Link to="/dashboard/notas">
-          <ButtonWithIcon
-            icon={<img src={Images.icons.IconEdit} alt="Notas" />}
-            position="left"
-            text={
-              user && user.role === "AMBASSADOR" ? "Notas privadas" : "Notas"
-            }
-            size="block"
-            type="secondary"
-          />
-        </Link>
+        <ButtonWithIcon
+          icon={<img src={Images.icons.IconEdit} alt="Notas" />}
+          position="left"
+          text={user && user.role === "AMBASSADOR" ? "Notas privadas" : "Notas"}
+          size="block"
+          type="secondary"
+          onClick={() => {
+            navigate("/dashboard/notas");
+            window.location.reload();
+          }}
+        />
       </div>
     </div>
   );
