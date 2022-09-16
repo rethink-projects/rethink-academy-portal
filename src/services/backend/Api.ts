e02db77d8858169c9e9a6220fe61e0d0a3d00545
Gabriel Gomes <gabriel.gomes@rethink.dev>
2022-09-16 12:15:45 -0300

    fix .env

--- src/services/backend/Api.ts
+++ src/services/backend/Api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.BACKEND_URL,
+  baseURL: process.env.REACT_APP_BACKEND_URL,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
